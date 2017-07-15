5ad2cdcdc88c635dc343381280a07b8ea32eb4d1
sushant12 <sussyoung9@gmail.com>
2017-07-15 10:34:44 +0545

    remove validation for contribution amt

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -17,7 +17,7 @@ class Reward < ActiveRecord::Base
   ranks :row_order, with_same: :project_id
 
   validates_presence_of :minimum_value, :description, :deliver_at #, :days_to_delivery
-  validates_numericality_of :minimum_value, greater_than_or_equal_to: 10.00, message: 'Amount must be greater than or equal to Rs 100'
+  # validates_numericality_of :minimum_value, greater_than_or_equal_to: 10.00, message: 'Amount must be greater than or equal to Rs 100'
 
   validates_numericality_of :maximum_contributions, only_integer: true, greater_than: 0, allow_nil: true
   scope :remaining, -> {
