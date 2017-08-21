a2db606377a4d95152e195dcd144bbfd299cddd9
sushant12 <sussyoung9@gmail.com>
2017-08-21 22:01:59 +0545

    test delivery url for stupid sct

--- app/controllers/sct_controller.rb
+++ app/controllers/sct_controller.rb
@@ -22,6 +22,26 @@ class SctController < ApplicationController
   end
 
   def thanks
-    redirect_to project_contribution_url(session[:project_id],session[:contribution_id])
+    # Grab POST variables
+    transaction_id = params["MERCHANTTXNID"]
+    ref_no = params["GTWREFNO"]	# Reference no provided by gateway
+    merchant_username = 'grasruts_uat'
+    merchant_password = CatarseSettings[:api_password]
+    signature_passcode = CatarseSettings[:signature]
+    # transaction_id = Time.now.to_i.to_s
+    password = Digest::SHA256.hexdigest(merchant_username+merchant_password)
+    sign = Digest::SHA256.hexdigest(signature_passcode+merchant_username+transaction_id)
+    client = Savon.client(wsdl: 'https://gateway.sandbox.npay.com.np/websrv/Service.asmx?wsdl')
+    @params = {
+        "MerchantId" => 169,
+        "MerchantTxnId" => transaction_id,
+        "MerchantUserName" => merchant_username,
+        "MerchantPassword" => password,
+        "Signature" => sign,
+        "GTWREFNO" => ref_no
+    }
+    @response = client.call(:CheckTransactionStatus, message: @params)
+    # @process_id = response.body[:validate_merchant_response][:validate_merchant_result][:processid]
+    # redirect_to project_contribution_url(session[:project_id],session[:contribution_id])
   end
 end
