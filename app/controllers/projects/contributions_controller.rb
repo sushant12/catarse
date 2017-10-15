cac3569c31186b90366b5a889a1844bb9ca9b474
sushant12 <sussyoung9@gmail.com>
2017-10-15 20:21:04 +0545

    [WIP] fixing payment sys

--- app/controllers/projects/contributions_controller.rb
+++ app/controllers/projects/contributions_controller.rb
@@ -5,7 +5,7 @@ class Projects::ContributionsController < ApplicationController
   inherit_resources
   actions :index, :show, :new, :update, :review, :create
   skip_before_filter :verify_authenticity_token, only: [:moip]
-  after_filter :verify_authorized, except: [:index]
+  after_filter :verify_authorized, except: [:index, :payment_method,:payment_redirect]
   belongs_to :project
   before_filter :detect_old_browsers, only: %i[new create]
 
@@ -25,6 +25,21 @@ class Projects::ContributionsController < ApplicationController
     resource.update_attributes(permitted_params)
     resource.update_user_billing_info
     render json: { message: 'updated' }
+  end
+
+  def payment_method
+  end
+
+  def payment_redirect
+    if params[:type] == 'sct'
+      @process_id = sct_init
+      binding.pry
+      render 'sct/index'
+    elsif params[:type] == 'esewa'
+    elsif params[:type] == 'card'
+    else
+      raise 'Unrecognised Payment Processor '
+    end
   end
 
   def show
@@ -154,5 +169,26 @@ class Projects::ContributionsController < ApplicationController
 
   def engine
     PaymentEngines.find_engine('Pagarme')
+  end
+
+  def sct_init
+    merchant_username = 'grasruts_uat'
+    merchant_password = CatarseSettings[:api_password]
+    signature_passcode = CatarseSettings[:signature]
+    transaction_id = Time.now.to_i.to_s
+    password = Digest::SHA256.hexdigest(merchant_username+merchant_password)
+    sign = Digest::SHA256.hexdigest(signature_passcode+merchant_username+transaction_id)
+    client = Savon.client(wsdl: 'https://gateway.sandbox.npay.com.np/websrv/Service.asmx?wsdl')
+    @params = {
+        "MerchantId" => 169,
+        "MerchantTxnId" => transaction_id,
+        "MerchantUserName" => merchant_username,
+        "MerchantPassword" => password,
+        "Signature" => sign,
+        "AMOUNT" => session[:value],
+        "purchaseDescription" => "Contributed to #{session[:project_name]} by #{current_user.name} -- #{current_user.email}"
+    }
+    response = client.call(:validate_merchant, message: @params)
+    response.body[:validate_merchant_response][:validate_merchant_result][:processid]
   end
 end
